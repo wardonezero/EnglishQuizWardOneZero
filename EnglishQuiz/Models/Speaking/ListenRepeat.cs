efd3d3319425c54f9ec06f7cfe270a6d7593b063
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2026-05-28 08:10:55 +0400

    models

--- EnglishQuiz/Models/Speaking/ListenRepeat.cs
+++ EnglishQuiz/Models/Speaking/ListenRepeat.cs
@@ -2,6 +2,9 @@
 
 public class ListenRepeat : BaseQuiz
 {
+    public string MainText = string.Empty;
+    public string MainImageUrl = string.Empty;
+    public string MainAudioUrl = string.Empty;
     public string[] Sentences { get; set; } = [string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,];
     public string[] ImageUrls { get; set; } = [string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,];
     public string[] AudioUrls { get; set; } = [string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,];
---
+++ EnglishQuiz/Models/SpeechConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace EnglishQuiz.Models;
+
+public record SpeechConfiguration
+{
+    public string Key1 { get; set; } = string.Empty;
+    public string Key2 { get; set; } = string.Empty;
+    public string Location { get; set; } = string.Empty;
+    public string Endpoint { get; set; } = string.Empty;
+    public string[] Voices { get; set; } = [];
+}
