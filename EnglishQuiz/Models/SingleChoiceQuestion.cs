215216fc90928d7863da79b936daef87ac0526b1
Eduard Melik-Hakobyan <eduard.wintersnow@outlook.com>
2026-05-28 17:12:49 +0400

    single choice question student answer

--- EnglishQuiz/Models/SingleChoiceQuestion.cs
+++ EnglishQuiz/Models/SingleChoiceQuestion.cs
@@ -12,3 +12,15 @@ public class SingleChoiceQuestionAnswer
     public int Id { get; set; } = 0;
     public string Answer { get; set; } = string.Empty;
 }
+
+public class SingleChoiceQuestionStudentAnswer
+{
+    public int Id { get; set; } = 0;
+    public int StudentId { get; set; } = 0;
+    public string Answer { get; set; } = string.Empty;
+    public bool IsOmitted => string.IsNullOrWhiteSpace(Answer);
+    public string Question { get; set; } = string.Empty;
+    public string[] Options { get; set; } = [];
+    public string CorrectAnswer { get; set; } = string.Empty;
+    public bool IsCorrect => !string.IsNullOrWhiteSpace(CorrectAnswer) && !string.IsNullOrWhiteSpace(Answer) && Answer == CorrectAnswer;
+}
